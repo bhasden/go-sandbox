5f48d516566c4b109ca61b22f842b0fb18ecab60
Kevin Strong <kevin@gomerits.com>
2022-02-03 11:14:41 -0600

    add delete capability to cache

--- cache/cache.go
+++ cache/cache.go
@@ -7,19 +7,35 @@ import (
 )
 
 type Cache[E comparable] struct {
-	set *set.Set[E]
+	set           *set.Set[E]
+	validDuration time.Duration
 }
 
-func New[E comparable](expiration time.Duration) *Cache[E] {
+func NewWithExpiry[E comparable](expiration time.Duration) *Cache[E] {
+	return &Cache[E]{
+		set:           set.New[E](),
+		validDuration: expiration,
+	}
+}
+
+func New[E comparable]() *Cache[E] {
 	return &Cache[E]{
 		set: set.New[E](),
 	}
 }
 
-func (cache *Cache[E]) Add(elements ...E) {
+func (cache *Cache[E]) Contains(element E) bool {
+	return cache.set.Contains(element)
+}
 
+func (cache *Cache[E]) Add(elements ...E) {
+	cache.set.Add(elements...)
 }
 
 func (cache *Cache[E]) Members() []E {
 	return nil
+}
+
+func (cache *Cache[E]) Remove(element E) {
+	cache.set.Delete(element)
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -5,25 +5,71 @@ import (
 	"time"
 
 	"github.com/kevinstrong/cache"
-
-	"github.com/google/go-cmp/cmp"
 )
 
 func TestCacheExpiration(t *testing.T) {
 	expiration := 1000 * time.Millisecond
-	c := cache.New[string](expiration)
+	c := cache.NewWithExpiry[string](expiration)
 
 	testString := "/v1/users/1"
 	c.Add(testString)
 
-	if !cmp.Equal(c.Members(), []string{testString}) {
-		t.Fatal(cmp.Diff(c.Members(), []string{testString}))
+	if !c.Contains(testString) {
+		t.Fatal("cache should contain testString after add")
 	}
 
-	time.Sleep(expiration)
+	time.Sleep(expiration + (10 * time.Millisecond))
 
-	if !cmp.Equal(c.Members(), []string{}) {
-		t.Fatal(cmp.Diff(c.Members(), []string{}))
+	if c.Contains(testString) {
+		t.Fatal("cache should remove testString after expiration duration")
+	}
+}
+
+func TestCacheGet_WithExistingValue_ReturnTrue(t *testing.T) {
+	c := cache.New[string]()
+	testString := "/v1/users/1"
+
+	isInBeforeAdd := c.Contains(testString)
+	if isInBeforeAdd {
+		t.Fatal("value is present in cache before add is called")
 	}
 
+	c.Add(testString)
+	isInCache := c.Contains(testString)
+
+	if !isInCache {
+		t.Fatal("test string should be in cache")
+	}
+}
+
+func TestCacheGet_WithNonExistingValue_ReturnFalse(t *testing.T) {
+	c := cache.New[string]()
+	testString := "/v1/users/1"
+
+	isInBeforeAdd := c.Contains(testString)
+	if isInBeforeAdd {
+		t.Error("value is present in cache before add is called")
+	}
+
+	isInCache := c.Contains(testString)
+
+	if isInCache {
+		t.Fatal("test string should not be in cache")
+	}
+}
+
+func TestCacheRemove(t *testing.T) {
+	c := cache.New[int]()
+	testValue := 1
+
+	c.Add(testValue)
+	if !c.Contains(testValue) {
+		t.Fatal("test value should be in cache after add is called")
+	}
+
+	c.Remove(testValue)
+
+	if c.Contains(testValue) {
+		t.Fatal("test value still present after remove is called")
+	}
 }
