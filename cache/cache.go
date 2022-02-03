0e5e5c1da04303d05ea3e2a1ef571c30049061fe
Kevin Strong <kevin@gomerits.com>
2022-02-03 11:49:41 -0600

    add cache expiry logic

--- cache/cache.go
+++ cache/cache.go
@@ -9,12 +9,14 @@ import (
 type Cache[E comparable] struct {
 	set           *set.Set[E]
 	validDuration time.Duration
+	cancelMap     map[E]chan struct{}
 }
 
 func NewWithExpiry[E comparable](expiration time.Duration) *Cache[E] {
 	return &Cache[E]{
 		set:           set.New[E](),
 		validDuration: expiration,
+		cancelMap:     map[E]chan struct{}{},
 	}
 }
 
@@ -28,8 +30,30 @@ func (cache *Cache[E]) Contains(element E) bool {
 	return cache.set.Contains(element)
 }
 
+// this blows up in a concurrent environment
 func (cache *Cache[E]) Add(elements ...E) {
-	cache.set.Add(elements...)
+	for _, value := range elements {
+		value := value
+
+		cancelChan, ok := cache.cancelMap[value]
+		if ok {
+			close(cancelChan)
+		}
+
+		cache.set.Add(value)
+
+		timer := time.NewTimer(cache.validDuration)
+		cancelChan = make(chan struct{})
+		cache.cancelMap[value] = cancelChan
+		go func() {
+			select {
+			case <-timer.C:
+				cache.set.Delete(value)
+			case <-cancelChan:
+				return
+			}
+		}()
+	}
 }
 
 func (cache *Cache[E]) Members() []E {
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestCacheExpiration(t *testing.T) {
-	expiration := 1000 * time.Millisecond
+	expiration := 5 * time.Millisecond
 	c := cache.NewWithExpiry[string](expiration)
 
 	testString := "/v1/users/1"
@@ -25,6 +25,30 @@ func TestCacheExpiration(t *testing.T) {
 	}
 }
 
+func TestCacheExpiration_DoubleAdd(t *testing.T) {
+	expiration := 1000 * time.Millisecond
+	c := cache.NewWithExpiry[string](expiration)
+
+	testString := "/v1/users/1"
+	c.Add(testString)
+	time.Sleep(500 * time.Millisecond)
+
+	c.Add(testString)
+
+	time.Sleep(600 * time.Millisecond)
+
+	if !c.Contains(testString) {
+		t.Fatal("cache failed to reset expiration timer on a 2nd add")
+	}
+
+	time.Sleep(400*time.Millisecond + 10*time.Millisecond)
+
+	if c.Contains(testString) {
+		t.Fatal("cache failed to remove updated value from 2nd add")
+	}
+}
+
+// our code demands an expiry time currently, please fix.
 func TestCacheGet_WithExistingValue_ReturnTrue(t *testing.T) {
 	c := cache.New[string]()
 	testString := "/v1/users/1"
